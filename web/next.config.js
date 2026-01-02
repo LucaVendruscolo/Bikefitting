db6d183687a29acc537ed2e1387b08ceb28e1460
Luca Vendruscolo <lucavendruscolo3@gmail.com>
2026-01-02 20:35:30 +0000

    Remove static export - use Vercel native Next.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  // ONNX Runtime is loaded from CDN, so we don't need special webpack config
 };
 
 module.exports = nextConfig;
